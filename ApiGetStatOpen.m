ab6120bcb5a598b67765055fc93efdf026fef790
a100kpm <iannis3toussaint@gmail.com>
2019-05-11 14:08:49 +0200

    Update ApiGetStatOpen.m

--- ApiGetStatOpen.m
+++ ApiGetStatOpen.m
@@ -1,5 +1,5 @@
 function data=ApiGetStatOpen(id_game)
-% not my code (thx shiba)
+
 id_game_rq=num2str(round(id_game));
 RQString=['https://api.opendota.com/api/matches/',id_game_rq];
 options = weboptions('Timeout',60);
